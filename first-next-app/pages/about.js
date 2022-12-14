2b8668824aa8098e17b59689de403bab831012c0
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-14 13:07:20 +0530

    Added about page

---
+++ first-next-app/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(){
+    return(
+        <div>
+            <h1>Inside About page.</h1>
+            <p>About Next.js</p>
+        </div>
+    )
+}
+
+export default About;
