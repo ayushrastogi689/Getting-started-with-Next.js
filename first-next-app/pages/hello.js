391a1ed01faa97c166dd298e9ecce5a8a2478c7b
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-14 13:06:52 +0530

    Added a new page hello

---
+++ first-next-app/pages/hello.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const hello = () => {
+  return (
+    <div>
+        <h1>Hello Next.js</h1>
+        <h2>Added Lorem Ipsum</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta quae illum natus aperiam quam ducimus minima qui asperiores. Hic vitae dolor excepturi inventore. Delectus aut qui sint aliquid sit ipsam.</p>
+    </div>
+  )
+}
+
+export default hello
