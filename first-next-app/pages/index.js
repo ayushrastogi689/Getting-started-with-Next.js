e561cab5ed561326c9707155def752834aeabf9c
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-15 11:48:28 +0530

    Created learnNext.js

--- first-next-app/pages/index.js
+++ first-next-app/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from "next/link"
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -22,10 +23,12 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+          <Link href="/learn/learnNext">
+          <a className={styles.card}>
+            <h2>Learn Next.js &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
+          </Link>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
---
+++ first-next-app/pages/learn/learnNext.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Next = () => {
+  return (
+    <div>
+        <h1>Inside Learn folder</h1>
+        <p>Learn Next.js</p>
+    </div>
+  )
+}
+
+export default Next
