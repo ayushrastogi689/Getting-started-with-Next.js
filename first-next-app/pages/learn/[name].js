5cf384b53125f99b15561b585889990e4655396f
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-15 14:55:18 +0530

    Understanding dynamic route using useRouter() hook

---
+++ first-next-app/pages/learn/[name].js
@@ -0,0 +1,25 @@
+// Topic : Dynamic Routes
+
+// Suppose you want to talk about a 10-100 different technologies then defining routes by predefining paths will be a hectic task as you need to manually define the path for each page/file. 
+// Writing code for each file again and again is not a good idea.
+
+// In Next.js we can add [] to the file name like this - [learnReact].js . This allows us to create a Dynamic Routes
+
+// For this we will use a Next.js hook useRouter
+
+import React from 'react'
+
+import {useRouter} from "next/router"
+
+function Learn() {
+    const router = useRouter();
+    const {name} = router.query
+  return (
+    <div>
+        <h1>Inside Learn folder</h1>
+        <p>Learn Angular</p>
+    </div>
+  )
+}
+
+export default Learn
