9593b50eca416ba6b29fc940fd155226f91f2f05
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-15 12:31:12 +0530

    Added Learn React and Lear Vue pages

---
+++ first-next-app/pages/learn/learnReact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const learnReact = () => {
+  return (
+    <div>
+        <h1>Inside Learn folder</h1>
+        <p>Learn React.js</p>
+    </div>
+  )
+}
+
+export default learnReact
---
+++ first-next-app/pages/learn/learnVue.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const learnVue = () => {
+  return (
+    <div>
+        <h1>Inside Learn folder</h1>
+        <p>Learn Vue.js</p>
+    </div>
+  )
+}
+
+export default learnVue
