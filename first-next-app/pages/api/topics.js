f2651634140f89e4fa9eded3876eb5570a0c1b71
Ayush Rastogi <ayushrastogi689@gmail.com>
2022-12-16 23:32:23 +0530

    Created an array of items

---
+++ first-next-app/pages/api/topics.js
@@ -0,0 +1,19 @@
+export default list[
+    {
+        id:"next",
+        about: "Next.js is a FE framework built on top of React, which gives the best developer experience"
+    },
+    {
+        id: "react",
+        about: "React is an open source Javascript library built for FE"
+    },
+    {
+        id: "vue",
+        about: "Vue is a progressive framework for building user interface"
+    },
+    {
+        id: "angular",
+        about: "Angular is a development platform built on Typescript"
+
+    }
+]
